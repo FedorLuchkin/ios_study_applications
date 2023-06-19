950a5a5b8bd5e51420e89f5c13d04413711fe3a6
FedorLuchkin <f.luchkin@mail.ru>
2023-06-19 09:25:59 +0300

    CoreDataDemo_without_StorageManager_using

---
+++ CoreDataDemo/CoreDataDemo/TaskListViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Fixed on 18.06.2023.
+//
+
+import UIKit
+import CoreData
+
+protocol TaskTaskViewControllerDelegate {
+    func reloadData()
+}
+
+class TaskListViewController: UITableViewController {
+    
+    private var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private let cellID = "cell"
+    private var taskList: [Task] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        setupNavigationBar()
+        fetchData()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarApperance = UINavigationBarAppearance()
+        navBarApperance.configureWithOpaqueBackground()
+        
+        navBarApperance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarApperance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarApperance.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 194/255)
+        
+        navigationController?.navigationBar.standardAppearance = navBarApperance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarApperance
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewTask))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewTaskAlertVersion))
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    @objc private func addNewTask() {
+        let taskVC = TaskViewController()
+        taskVC.delegate = self
+        present(taskVC, animated: true)
+    }
+    
+    @objc private func addNewTaskAlertVersion() {
+        showAlert(with: "New Task", and: "What do you want to do?")
+    }
+    
+    private func fetchData() {
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            taskList = try context.fetch(fetchRequest)
+            print("NICE@@@@")
+            print(taskList.count)
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            self.save(task)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New Task"
+        }
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        guard let task = NSManagedObject(entity: entityDescription, insertInto: context) as? Task else { return }
+        task.title = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+                
+        }
+    }
+
+}
+
+// MARK: - UITableViewDataSource
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+}
+
+// MARK: - TaskTaskViewControllerDelegate
+extension TaskListViewController: TaskTaskViewControllerDelegate {
+    func reloadData() {
+        fetchData()
+        tableView.reloadData()
+    }
+}
